9ad28bb5a68943daafbfc7df7fa940447bdf04f8
Askar Azhibaev <86300778+9o1d@users.noreply.github.com>
2023-12-21 20:49:29 +0700

    Add files via upload

---
+++ shutdown/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    private static Timer timer;
+
+    static void Main(string[] args)
+    {
+        var timerState = new TimerState { Counter = 0 };
+
+        timer = new Timer(
+            callback: new TimerCallback(TimerTask),
+            state: timerState,
+            dueTime: 0,
+            period: 300000);
+
+        while (true)
+        {
+            Task.Delay(1000).Wait();
+        }
+
+        timer.Dispose();
+        Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff}: done.");
+    }
+
+    private static void TimerTask(object timerState)
+    {
+        WebClient client = new WebClient();
+        string s = client.DownloadString("https://example.com/status.php");
+        int i = int.Parse(s);
+        Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff} : " + i);
+        if (i == 1)
+        {
+            Process.Start("shutdown", "/g /f /t 5");
+        }
+    }
+
+    class TimerState
+    {
+        public int Counter;
+    }
+}
---
+++ shutdown/status.php
@@ -0,0 +1,22 @@
+<?php
+$s_file_name = "/var/www/html/status.txt";
+if (isset($_GET['i']))
+{
+	$i = intval($_GET['i']);
+	file_put_contents($s_file_name, $i);
+	echo $i;
+}
+else
+{
+	$s = @file_get_contents($s_file_name);
+	if (!empty($s))
+	{
+		echo intval($s);
+		file_put_contents($s_file_name, "0");
+	}
+	else
+	{
+		echo 0;
+	}
+}
+?>
